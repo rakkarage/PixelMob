b1085788c9c1f9ff4dc5c0e1bd7be8b701feab92
Rakka Rage <rakkarage@gmail.com>
2020-04-08 20:45:23 -0400

    x

--- PixelMob/Mob.cs
+++ PixelMob/Mob.cs
@@ -1,11 +1,33 @@
 using Godot;
+using static Godot.GD;
 public class Mob : Sprite
 {
+	private AnimationNodeStateMachinePlayback _playback;
 	public override void _Ready()
 	{
-
+		Print("!!!ready");
+		var player = GetNode<AnimationPlayer>("AnimationPlayer");
+		var tree = GetNode<AnimationTree>("AnimationTree");
+		_playback = (AnimationNodeStateMachinePlayback)tree.Get("parameters/playback");
+		_playback.Start("Idle");
 	}
 	//  public override void _Process(float delta)
 	//  {
 	//  }
+	public void OnAnimationTreeEntered()
+	{
+		Print("!!!entered");
+	}
+	public void OnAnimationTreeExited()
+	{
+		Print("!!!exited");
+	}
+	public void OnAnimationTreeExiting()
+	{
+		Print("!!!exiting");
+	}
+	public void OnAnimationFinished()
+	{
+		Print("!!!finished");
+	}
 }
