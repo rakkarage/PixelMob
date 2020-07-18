525ce7e1e8eaacb9195cb286ba190186adcc6efe
Rakka Rage <rakkarage@gmail.com>
2020-07-18 13:06:23 -0400

    add more mobs

--- PixelMob/Mob.gd
+++ PixelMob/Mob.gd
@@ -1,7 +1,7 @@
 extends Control
 
 onready var _attack := $Panel/HBox/Attack
-onready var _move := $Panel/HBox/Move
+onready var _walk := $Panel/HBox/Walk
 onready var _player := $Sprite/AnimationPlayer
 onready var _tree := $Sprite/AnimationTree
 onready var _machine
@@ -10,10 +10,10 @@ func _ready() -> void:
 	_machine = _tree.get("parameters/playback")
 	_machine.start("Idle")
 	Utility.ok(_attack.connect("pressed", self, "_attackPressed"))
-	Utility.ok(_move.connect("pressed", self, "_walkPressed"))
+	Utility.ok(_walk.connect("pressed", self, "_walkPressed"))
 
 func _attackPressed() -> void:
 	_machine.travel("Attack")
 
-func _movePressed() -> void:
+func _walkPressed() -> void:
 	_machine.travel("Walk")
