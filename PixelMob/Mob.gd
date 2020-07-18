349cf0dce0ae2bd4db6520235af4016d1c2e0f2f
Rakka Rage <rakkarage@gmail.com>
2020-07-18 16:08:26 -0400

    x

--- PixelMob/Mob.gd
+++ PixelMob/Mob.gd
@@ -2,20 +2,22 @@ extends Control
 
 onready var _attack := $Panel/HBox/Attack
 onready var _walk := $Panel/HBox/Walk
-onready var _player := $Sprite/AnimationPlayer
-onready var _tree := $Sprite/AnimationTree
+onready var _player: AnimationPlayer = $Sprite/AnimationPlayer
+onready var _tree: AnimationTree = $Sprite/AnimationTree
 onready var _machine: AnimationNodeStateMachinePlayback = _tree.get("parameters/playback")
 
 # TODO: how to make the blend tree pick a random animation when i get back to idle?
+# TODO: how to be notified of animation finished or changed? so i can set the blend tree input to a random variable?
 
 func _ready() -> void:
+	_machine.start("Idle")
 	Utility.ok(_attack.connect("pressed", self, "_attackPressed"))
 	Utility.ok(_walk.connect("pressed", self, "_walkPressed"))
+	# FIXME: none of these are called!?
 	Utility.ok(_player.connect("animation_started", self, "_snimationStarted"))
 	Utility.ok(_player.connect("animation_changed", self, "_snimationChanged"))
 	Utility.ok(_player.connect("animation_finished", self, "_animationFinished"))
 	Utility.ok(_player.connect("caches_cleared", self, "_animationCache"))
-	_machine.start("Idle")
 
 func _attackPressed() -> void:
 	_machine.travel("Attack")
