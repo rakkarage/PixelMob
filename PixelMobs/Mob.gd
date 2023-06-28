b330f58916d99a698e8a3045a41d61f9889e18e3
Rakka Rage <rakkarage@gmail.com>
2023-06-28 13:45:11 -0400

    x

--- PixelMobs/Mob.gd
+++ PixelMobs/Mob.gd
@@ -1,20 +1,20 @@
-class_name Mob
 extends Sprite2D
+class_name Mob
 
 @onready var _tree: AnimationTree = $Tree
 @onready var _machine: AnimationNodeStateMachinePlayback = _tree.get("parameters/playback")
 const _key := "parameters/Idle/blend_position"
-const _idleAnimationPriority := {
+var _idle_animation_probability := {
 	-1: 100,
 	0: 20,
 	1: 1
 }
 
 func _ready():
-	_machine.start("Idle")
+	_machine.travel("Idle")
 
 func randomIdle() -> void:
-	_tree[_key] = Random.priority(_idleAnimationPriority)
+	_tree[_key] = Random.probability(_idle_animation_probability)
 
 func idle():
 	_machine.travel("Idle")
--- PixelMobs/Test.gd
+++ PixelMobs/Test.gd
@@ -6,15 +6,15 @@ extends Control
 @onready var _mob := $Panel/VBox/Center/Panel/Slime
 
 func _ready() -> void:
-	_attack.connect("pressed", _attackPressed)
-	_walk.connect("pressed", _walkPressed)
-	_turn.connect("pressed", _turnPressed)
+	_attack.connect("pressed", _attack_pressed)
+	_walk.connect("pressed", _walk_pressed)
+	_turn.connect("pressed", _turn_pressed)
 
-func _attackPressed() -> void:
+func _attack_pressed() -> void:
 	_mob.attack()
 
-func _walkPressed() -> void:
+func _walk_pressed() -> void:
 	_mob.walk()
 
-func _turnPressed() -> void:
+func _turn_pressed() -> void:
 	_mob.flip_h = not _mob.flip_h
